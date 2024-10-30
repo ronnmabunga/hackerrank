973fdbb70fae63c1ce67672f1552175783de3f3f
ronnmabunga <ronnmabunga@gmail.com>
2024-10-30 10:12:02 +0800

    Move Python challenges to subfolders

---
+++ challenges/Python/Sets/py-set-intersection-operation.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = input()
+n = set(map(int, input().split(' ')))
+b = input()
+b = set(map(int, input().split(' ')))
+print(len(n.intersection(b)))
